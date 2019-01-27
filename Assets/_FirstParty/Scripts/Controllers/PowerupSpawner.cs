5ca902d63e4a5bf7ceb8b5c764a58556375fe0cf
Hayden Reeve <39004735+Greysion@users.noreply.github.com>
2019-01-27 10:27:07 +0800

    Organised Script Folders
    • Removed Redundant Scripts.
    • Refactored PickpSpawn => PowerupSpawner.
    • Sub-Foldered scripts.

--- Assets/_FirstParty/Scripts/Controllers/PowerupSpawner.cs
+++ Assets/_FirstParty/Scripts/Controllers/PowerupSpawner.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PickupSpawn : MonoBehaviour
+public class PowerupSpawner : MonoBehaviour
 {
 
 	Camera mainCamera;
